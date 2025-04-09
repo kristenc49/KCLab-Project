34fd84da269fbf40afdbb34b49b4ccc8632c5eac
1N4 <sample@bths.edu>
2025-04-09 08:29:34 -0400

    Initial Commit

---
+++ KCLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KCLab
+//
+//  Created by Student on 4/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KCLab/KCLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  KCLabApp.swift
+//  KCLab
+//
+//  Created by Student on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct KCLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
