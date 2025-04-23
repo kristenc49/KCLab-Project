edf9e24b8d20282c52ffd6f9d7d46e52448ca371
caitlin3862 <caitlinl21@nycstudents.net>
2025-04-23 08:02:43 -0400

    added groceries + dragging feature

---
+++ KCLab/GroceryItem.swift
@@ -0,0 +1,8 @@
+//
+//  GroceryItem.swift
+//  KCLab
+//
+//  Created by Caitlin Lee on 4/23/25.
+//
+
+import Foundation
---
+++ KCLab/GroceryItemView.swift
@@ -0,0 +1,18 @@
+//
+//  GroceryItemView.swift
+//  KCLab
+//
+//  Created by Caitlin Lee on 4/23/25.
+//
+
+import SwiftUI
+
+struct GroceryItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroceryItemView()
+}
